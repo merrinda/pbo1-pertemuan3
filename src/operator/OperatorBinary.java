d3d2922ee7a3074f1614280c80bea3d1e040a0f3
merrinda <merrindaratih28@gmail.com>
2020-05-02 20:01:11 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+
+
+    String nama = "Merrinda Ratih Manjari";
+    int jumlahsks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+    
+}
