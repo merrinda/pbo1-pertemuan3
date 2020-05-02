c7643bbb5c1e05b2408a982c184cfa106ded19fc
merrinda <merrindaratih28@gmail.com>
2020-05-02 20:04:33 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -2,7 +2,7 @@ package operator;
 
 public class OperatorBinary {
 
-    //operator (=) sebagai assigment/penugasan
+    //operator (=) sebagai assignment/penugasan
     //menempatkan nilai kedalam variable
 
 
@@ -10,5 +10,13 @@ public class OperatorBinary {
     int jumlahsks = 24;
     double indeksPrestasiKumulatif = 3.89;
 
-    
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+
 }
