3a93a0fd6695dba77447c16f8a705377e8766eea
merrinda <merrindaratih28@gmail.com>
2020-05-03 15:40:49 +0800

    Percabangan switch uwuuu

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanjalan=0;
+        switch(jalanjalan){
+            case 1:
+                System.out.println("single");
+                break;
+            case 2:
+                System.out.println("Dated");
+                break;
+            case 3:
+                System.out.println("kacang");
+                break;
+            case 4:
+                System.out.println("dinner");
+                break;
+                default:
+                    System.out.println("tak kasat mata uwu");
+                    break;
+        }
+    }
+}
